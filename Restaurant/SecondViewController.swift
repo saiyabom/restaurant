b3edeb4424b2e100a9853ae744b861d8827ad801
user <user@users-macbook-pro.local>
2016-02-16 22:38:07 +0700

    Initial Commit

---
+++ Restaurant/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Restaurant
+//
+//  Created by user on 2/16/16.
+//  Copyright © 2016 Imsi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
